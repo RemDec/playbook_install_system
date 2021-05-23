a4eb9175a64924b42340bb266b5a4e33aec1b9d5
Remdec <remydecocq@gmail.com>
2021-05-23 08:28:54 +0200

    Correct tasks following executed test on Debian VM

--- launch_playbook.sh
+++ launch_playbook.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
 if [ "$1" == "dry" ]; then
-  ansible-playbook site.yml -i inventory --check
+  ansible-playbook site.yml -i inventory -u root -k --check
+else
+  ansible-playbook site.yml -i inventory -u root -k
 fi
