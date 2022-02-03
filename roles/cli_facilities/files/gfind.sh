77d040e29bf3f93c3e5871ed1a250e485dcb2972
rde <rde@escaux.com>
2022-02-03 11:13:18 +0100

    Improve cli_facilities

--- roles/cli_facilities/files/gfind.sh
+++ roles/cli_facilities/files/gfind.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ -z "$1" ]; then
     echo "Usage :"
-    echo "   $0 pattern : do a recursive case-insensitive EXTENDED grep on pattern from current directory"
+    echo "   $0 pattern [more_grep_opts] : do a recursive case-insensitive EXTENDED grep on pattern from current directory"
     exit 1
 fi
 
-grep -Ri -E "$1" *
+grep -RIin$2 -E "$1" * | awk -F: '{print $1, $2"|"$3}'
